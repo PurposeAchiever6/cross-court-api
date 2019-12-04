feba11997b377ee34f751bc55e8be082b52e9303
Gabriel <gabibur028@gmail.com>
2019-12-04 13:59:00 -0300

    Purchase history

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2019_12_03_175544) do
-=======
-ActiveRecord::Schema.define(version: 2019_12_03_145649) do
->>>>>>> Change subscriptions to one time payments
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
