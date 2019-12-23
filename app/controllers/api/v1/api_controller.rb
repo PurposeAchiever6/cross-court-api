986dbc09afa6bf7ed135f3ed750f951c55835a50
Gabriel <gabibur028@gmail.com>
2019-12-23 16:58:10 -0300

    Sem check in users

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -15,7 +15,10 @@ module Api
       rescue_from InvalidDateException,                with: :render_custom_exception
       rescue_from NotEnoughCreditsException,           with: :render_custom_exception
       rescue_from UnauthorizedException,               with: :render_custom_exception
+<<<<<<< HEAD
       rescue_from InvalidActionException,              with: :render_custom_exception
+=======
+>>>>>>> Sem check in users
       rescue_from Stripe::InvalidRequestError,         with: :render_custom_exception
 
       def status
