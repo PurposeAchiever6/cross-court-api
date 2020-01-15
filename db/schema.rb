e9b4df965adcefa363ce23f513b4f273f63d0cc0
Gabriel <gabibur028@gmail.com>
2020-01-15 10:44:24 -0300

    Allow nil values on session_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_08_173207) do
+ActiveRecord::Schema.define(version: 2020_01_15_134202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,7 +104,7 @@ ActiveRecord::Schema.define(version: 2020_01_08_173207) do
 
   create_table "referee_sessions", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.bigint "session_id", null: false
+    t.bigint "session_id"
     t.date "date", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -115,7 +115,7 @@ ActiveRecord::Schema.define(version: 2020_01_08_173207) do
 
   create_table "sem_sessions", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.bigint "session_id", null: false
+    t.bigint "session_id"
     t.date "date", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
