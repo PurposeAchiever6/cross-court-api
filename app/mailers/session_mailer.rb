a25a73b2a9226d2f9dbf4ef349e4501286237ba2
Gabriel <gabibur028@gmail.com>
2020-01-15 11:53:17 -0300

    Fix confirmation url

--- app/mailers/session_mailer.rb
+++ app/mailers/session_mailer.rb
@@ -1,5 +1,6 @@
 class SessionMailer < ApplicationMailer
   HOUR_FORMAT = '%H:%M'.freeze
+  DATE_FORMAT = '%Y-%m-%d'.freeze
 
   def new_session(user, time, date, user_session_id)
     @user_name = user.name
@@ -15,7 +16,7 @@ class SessionMailer < ApplicationMailer
   def reminder(email, user_name, session_id, date)
     @user_name = user_name
     formatted_date = date.strftime(Session::DATE_FORMAT)
-    @confirmation_url = "#{ENV['FRONTENT_URL']}/sessions/#{session_id}?date=#{formatted_date}"
+    @confirmation_url = "#{ENV['FRONTENT_URL']}/sessions/#{session_id}/#{formatted_date}"
     mail(
       to: email,
       subject: t('mailer.session.reminder.subject')
