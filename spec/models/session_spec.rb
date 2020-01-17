c38569cda1685764867d08ce68465af2da2f2899
Gabriel <gabibur028@gmail.com>
2020-01-17 15:55:01 -0300

    Fix recurring update

--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -24,4 +24,44 @@ describe Session do
     it { is_expected.to validate_presence_of(:time) }
     it { is_expected.to belong_to(:location) }
   end
+
+  describe 'callbacks' do
+    let!(:session)               { create(:session, :daily) }
+    let!(:yesterday_sem_session) { create(:sem_session, session: session, date: Date.yesterday) }
+    let(:new_recurring_rule)     { IceCube::Rule.weekly }
+    let(:user)                   { create(:user) }
+    before do
+      8.times do |i|
+        create(:sem_session, session: session, date: Date.current + i.days)
+        create(:referee_session, session: session, date: Date.current + i.days)
+        create(:user_session, session: session, user: user, date: Date.current + i.days)
+      end
+    end
+
+    context 'when updating the recurring rule' do
+      it 'destroys all future sem_sessions which not occur in the new rule' do
+        expect {
+          session.update!(recurring: new_recurring_rule)
+        }.to change(SemSession, :count).by(-6)
+      end
+
+      it 'destroys all future referee_sessions which not occur in the new rule' do
+        expect {
+          session.update!(recurring: new_recurring_rule)
+        }.to change(RefereeSession, :count).by(-6)
+      end
+
+      it 'destroys all future user_sessions which not occur in the new rule' do
+        expect {
+          session.update!(recurring: new_recurring_rule)
+        }.to change(UserSession, :count).by(-6)
+      end
+
+      it 'adds credits to the user corresponding to the deleted user_sessions' do
+        expect {
+          session.update!(recurring: new_recurring_rule)
+        }.to change { user.reload.credits }.from(0).to(6)
+      end
+    end
+  end
 end
