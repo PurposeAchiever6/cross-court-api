433f9dac8820bdf038a81f66c734ab5d2b0d093d
Gabriel <gabibur028@gmail.com>
2020-05-11 16:57:44 -0300

    Fix promo codes

--- app/admin/promo_code.rb
+++ app/admin/promo_code.rb
@@ -1,12 +1,12 @@
 ActiveAdmin.register PromoCode do
-  permit_params :type, :code, :discount
+  permit_params :type, :code, :discount, :expiration_date
 
   form do |f|
     f.inputs 'Promo Code Details' do
       f.input :type, as: :select, collection: PromoCode::TYPES
       f.input :code
       f.input :discount
-      f.input :expiration_date
+      f.input :expiration_date, as: :datepicker, datepicker_options: { min_date: Date.current }
     end
     f.actions
   end
--- spec/requests/api/v1/promo_codes/show_spec.rb
+++ spec/requests/api/v1/promo_codes/show_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 describe 'GET api/v1/promo_code' do
+  let(:user)       { create(:user) }
   let(:promo_code) { create(:promo_code, discount: 10) }
   let(:price)      { 100 }
   let(:params)     { { promo_code: code, price: price } }
 
-  subject { get api_v1_promo_code_path, params: params, as: :json }
+  subject { get api_v1_promo_code_path, params: params, headers: auth_headers, as: :json }
 
   context "when the promo_code doesn't exists" do
     let(:code) { '12345678' }
@@ -22,6 +23,16 @@ describe 'GET api/v1/promo_code' do
     it 'returns the price with the promo_code applied' do
       subject
       expect(json[:price]).to eq(promo_code.apply_discount(price))
+    end
+  end
+
+  context 'when the promo code is invalid' do
+    let!(:user_promo_code) { UserPromoCode.create!(user: user, promo_code: promo_code) }
+    let(:code) { promo_code.code }
+
+    it 'returns promo_code invalid error message' do
+      subject
+      expect(json[:error]).to eq(I18n.t('api.errors.promo_code.invalid'))
     end
   end
 end
