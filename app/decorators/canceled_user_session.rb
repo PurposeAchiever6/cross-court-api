19fd48ff9e3c7d5bec507e8846d1d6834148a8ec
Gabriel <gabibur028@gmail.com>
2020-05-27 14:38:05 -0300

    Send slack notification when a user cancels

--- app/decorators/canceled_user_session.rb
+++ app/decorators/canceled_user_session.rb
@@ -12,6 +12,9 @@ class CanceledUserSession
       user.increment(:credits)
       user.save!
       user_session.credit_reimbursed = true
+      SlackService.new.session_canceled_in_time(user, date, time, location)
+    else
+      SlackService.new.session_canceled_out_of_time(user, date, time, location)
     end
     user_session.state = :canceled
     user_session.save!
