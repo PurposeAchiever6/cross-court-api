118da1ccde178a9b015771dd21cd1a59eb9f23de
Gabriel <gabibur028@gmail.com>
2020-05-28 15:29:17 -0300

    Remove negative answers from sms

--- app/services/sonar_service.rb
+++ app/services/sonar_service.rb
@@ -24,16 +24,6 @@ module SonarService
       if positive_message?(message)
         UserSessionConfirmed.new(user_session).save!
         send_message(user, I18n.t('notifier.session_confirmed'))
-      elsif negative_message?(message)
-        CanceledUserSession.new(user_session).save!
-        message = if user_session.in_cancellation_time?
-                    I18n.t('notifier.session_canceled_in_time', schedule_url: ENV['SCHEDULE_URL'])
-                  else
-                    I18n.t('notifier.session_canceled_out_of_time',
-                           schedule_url: ENV['SCHEDULE_URL'],
-                           cancellation_period: ENV['CANCELLATION_PERIOD'])
-                  end
-        send_message(user, message)
       else
         send_message(user, I18n.t('notifier.unreadable_text'))
       end
@@ -44,9 +34,5 @@ module SonarService
 
   def positive_message?(message)
     %w[yes y].include?(message.downcase)
-  end
-
-  def negative_message?(message)
-    %w[no n].include?(message.downcase)
   end
 end
