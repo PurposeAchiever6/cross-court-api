4ebbc9caa7be73b2cf6586c8fa43c472f38ea0b3
Matteo Guerrieri <matteo.guerrieri@rootstrap.com>
2021-10-15 21:57:08 -0300

    Fix auto-confirm job

--- app/jobs/confirm_unconfirmed_sessions_job.rb
+++ app/jobs/confirm_unconfirmed_sessions_job.rb
@@ -3,7 +3,7 @@ class ConfirmUnconfirmedSessionsJob < ApplicationJob
 
   def perform
     UserSessionsQuery.new.finished_cancellation_time.reserved.find_each do |user_session|
-      UserSessionAutoConfirmed.new(user_session)
+      UserSessionAutoConfirmed.new(user_session).save!
     end
   end
 end
