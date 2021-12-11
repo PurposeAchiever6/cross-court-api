81dc9f9064189df03d1390b17ed6a42aa6a842c4
Matteo Guerrieri <matteo.guerrieri@rootstrap.com>
2021-12-11 19:33:27 -0300

    New template for re confirmation instructions

--- app/mailers/devise_mailer.rb
+++ app/mailers/devise_mailer.rb
@@ -1,3 +1,15 @@
 class DeviseMailer < Devise::Mailer
   default 'Message-ID' => -> { "<#{SecureRandom.uuid}@#{ENV['SERVER_URL']}>" }
+  default template_path: 'devise/mailer'
+
+  def confirmation_instructions(record, token, options = {})
+    options[:template_name] =
+      if record.confirmation_sent_at <= Date.yesterday
+        're_confirmation_instructions'
+      else
+        'confirmation_instructions'
+      end
+
+    super
+  end
 end
