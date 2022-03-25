d5f058d395db2948195fcd51d25a776dc978a001
Ignacio Jorge <nacho.jorge94@gmail.com>
2022-03-25 17:01:05 -0300

    Add bulk create for gallery photos

--- app/admin/gallery_photo.rb
+++ app/admin/gallery_photo.rb
@@ -1,12 +1,19 @@
 ActiveAdmin.register GalleryPhoto do
+  actions :all, except: :edit
+
+  before_filter :skip_sidebar!, only: :index
+
   permit_params :image
 
   form do |f|
-    f.inputs 'Image' do
-      f.input :image, as: :file
+    f.inputs 'Images' do
+      f.input :image, label: 'Images', as: :file, input_html: { multiple: true, accept: 'image/*' }
     end
 
-    f.actions
+    f.actions do
+      f.action :submit, as: :button, label: 'Add Gallery Photos'
+      f.action :cancel, wrapper_html: { class: 'cancel' }, label: 'Cancel'
+    end
   end
 
   index do
@@ -26,6 +33,26 @@ ActiveAdmin.register GalleryPhoto do
         image_tag url_for(gallery_photo.image), class: 'max-h-100' if gallery_photo.image.attached?
       end
       row :created_at
+    end
+  end
+
+  controller do
+    def create
+      gallery_photo_params = params[:gallery_photo]
+
+      if gallery_photo_params.blank?
+        @resource = GalleryPhoto.new
+
+        flash.now[:error] = 'You need to select at least one photo to upload'
+
+        render :new
+      else
+        gallery_photo_params[:image].each do |image|
+          GalleryPhoto.create!(image: image)
+        end
+
+        redirect_to admin_gallery_photos_path
+      end
     end
   end
 end
