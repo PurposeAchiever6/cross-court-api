e0d0a814ac6a099466b6a79b65389c5573e60a97
Felipe Fava <felipe.fava@moove-it.com>
2022-05-15 18:42:14 -0300

    Fix checked in session notice email subject & content

--- app/mailers/session_mailer.rb
+++ app/mailers/session_mailer.rb
@@ -13,12 +13,14 @@ class SessionMailer < ApplicationMailer
     mail(to: @user_session.user_email, subject: I18n.t('mailer.session.booked'))
   end
 
-  def third_session_notice
+  def checked_in_session_notice
     @user = User.find(params[:user_id])
+    @number_of_sessions = params[:number_of_sessions]
 
     mail(
       to: [ENV['CC_TEAM_EMAIL'], ENV['CC_MKTG_EMAIL']],
-      subject: I18n.t('mailer.session.third_session_notice')
+      subject: I18n.t('mailer.session.checked_in_session_notice',
+                      number_of_sessions: @number_of_sessions)
     )
   end
 end
