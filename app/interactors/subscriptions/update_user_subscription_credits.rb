4cdfaf0e0c40b6c0db79af245ca5cd32fbe76bcf
Felipe Fava <felipe.fava@moove-it.com>
2022-07-20 13:45:21 -0300

    Reset subscription credits on subscription update

--- app/interactors/subscriptions/update_user_subscription_credits.rb
+++ app/interactors/subscriptions/update_user_subscription_credits.rb
@@ -4,21 +4,10 @@ module Subscriptions
 
     def call
       user = context.user
-      new_product = context.product
-      old_product = context.old_product
-      active_subscription = user.active_subscription
-      new_product_credits = new_product.credits
+      product = context.product
 
-      credits =
-        if !active_subscription || new_product.unlimited? || old_product.unlimited?
-          new_product_credits
-        else
-          used_credits = old_product.credits - user.subscription_credits
-          new_credits = new_product_credits - used_credits
-          new_credits.negative? ? 0 : new_credits
-        end
+      user.subscription_credits = product.credits
 
-      user.subscription_credits = credits
       user.save!
     end
   end
