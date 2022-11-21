66fa4ca02322067257326253d2490a086d53a61a
Colt McLaughlin <shuqinnq08@gmail.com>
2022-11-21 09:34:35 -0300

    Annotate missing models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -43,14 +43,14 @@
 #  cc_cash                                 :decimal(, )      default(0.0)
 #  source                                  :string
 #  reserve_team                            :boolean          default(FALSE)
-#  subscription_skill_session_credits      :integer          default(0)
 #  instagram_username                      :string
 #  first_time_subscription_credits_used_at :datetime
+#  subscription_skill_session_credits      :integer          default(0)
 #  flagged                                 :boolean          default(FALSE)
 #  is_coach                                :boolean          default(FALSE), not null
 #  gender                                  :integer
-#  credits_without_expiration              :integer          default(0)
 #  bio                                     :string
+#  credits_without_expiration              :integer          default(0)
 #  scouting_credits                        :integer          default(0)
 #
 # Indexes
