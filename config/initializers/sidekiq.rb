2ce154e62863e1ce75bc64857ced4b6f2460f374
Colt McLaughlin <shuqinnq08@gmail.com>
2023-01-26 16:16:21 -0300

    Fix sidekiq initializer

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,14 +1,16 @@
-schedule_file = 'config/schedule.yml'
-
-if File.exist?(schedule_file) && Sidekiq.server?
-  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
-end
-
 Sidekiq.configure_server do |config|
   config.redis = {
     url: ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379'),
     ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
   }
+
+  config.on(:startup) do
+    schedule_file = 'config/schedule.yml'
+
+    if File.exist?(schedule_file) && Sidekiq.server?
+      Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+    end
+  end
 end
 
 Sidekiq.configure_client do |config|
