172b072f879200333cdf8f36644c76a5cd10f793
Colt McLaughlin <shuqinnq08@gmail.com>
2023-05-04 10:02:08 -0300

    [992] - Update email confirmation email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,15 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "Crosscourt <#{ENV.fetch('CC_TEAM_EMAIL', nil)}>"
+  default 'Message-ID' => -> { "<#{SecureRandom.uuid}@#{ENV.fetch('SERVER_URL', nil)}>" }
+
   layout 'mailer'
+
+  helper do
+    def button_classes
+      'background-color: #9999ff; text-decoration: none; font-size: 16px; ' \
+        "font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; font-weight: 700; " \
+        'color: #000000; padding: 10px 25px; box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.25); ' \
+        'display: inline-block'
+    end
+  end
 end
